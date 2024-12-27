7f3277387ee175cc83f8b775a9b0ff8ac0fcc914
MIDLART <midlart@mail.ru>
2024-12-27 11:00:23 +0300

    Frontend

--- src/main/java/org/example/web/configurations/SecurityConfig.java
+++ src/main/java/org/example/web/configurations/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
   protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/registration").permitAll()
+                .requestMatchers("/", "/registration", "/static/**").permitAll()
                 .requestMatchers("/book/**", "/author/**")
                 .hasAnyAuthority("ROLE_ADMIN","ROLE_USER")
                 .anyRequest().authenticated()
--- src/main/java/org/example/web/controllers/BookTakingController.java
+++ src/main/java/org/example/web/controllers/BookTakingController.java
@@ -2,15 +2,20 @@ package org.example.web.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.example.web.models.Reader;
-import org.example.web.services.BookCopyService;
-import org.example.web.services.BookTakingService;
+import org.example.web.models.Author;
+import org.example.web.models.Genre;
+import org.example.web.services.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
 
 @Log4j2
 @Controller
@@ -18,26 +23,38 @@ import java.io.IOException;
 public class BookTakingController {
   private final BookTakingService bookTakingService;
   private final BookCopyService bookCopyService;
+  private final ReaderService readerService;
+  private final BookService bookService;
 
-  @PostMapping("/book_taking/save/{id}")
-  public String saveBookTaking(@PathVariable Integer id, Reader reader) throws IOException {
+//  @GetMapping("/search")
+//  public String readerSearch(@RequestParam(name = "searchWord", required = false) String title, Model model) {
+//    if (title == null || title.isEmpty()) {
+//      model.addAttribute("rbooks", "не найдено");
+//    }
+//    model.addAttribute("rbooks", bookService.listBooks(title));
+//    model.addAttribute("searchWord", title);
+//    return "rbooks";
+//  }
+
+  @PostMapping("/reader/{id}/book_taking/save/{book_id}")
+  public String saveBookTaking(@PathVariable Integer id, @PathVariable Integer bookId) throws IOException {
     try {
-      bookTakingService.saveBookTaking(bookCopyService.getBookCopyById(id), reader);
-      return "redirect:/book/{id}";
+      bookTakingService.saveBookTaking(bookCopyService.getBookCopyById(bookId), readerService.getReaderById(id));
+      return "redirect:/";
     } catch (Exception e) {
       log.error("Error creating bookCopy", e);
-      return "redirect:/book/{id}";
+      return "redirect:/";
     }
   }
 
   @Transactional
-  @PostMapping("/book_taking/delete/{id}")
-  public String deleteBookCopy(@PathVariable Integer id, Reader reader) {
+  @PostMapping("/reader/{id}/book_taking/delete/{book_id}")
+  public String deleteBookCopy(@PathVariable Integer id, @PathVariable Integer bookId) {
     try {
-      bookTakingService.endBookTaking(bookCopyService.getBookCopyById(id), reader);
+      bookTakingService.endBookTaking(bookCopyService.getBookCopyById(bookId), readerService.getReaderById(id));
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    return "redirect:/book/{id}";
+    return "redirect:/reader/{id}";
   }
 }
